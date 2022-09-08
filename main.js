c9d030fea7d0b77643b019b072b2d15877c0b118
BRMarceloBB <100157527+BRMarceloBB@users.noreply.github.com>
2022-09-08 15:59:09 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,23 @@
+function quarto1() {
+    window.location = "quarto.html";
+}
+
+function quarto2() {
+    window.location = "quarto2.html";
+}
+
+function doguineo() {
+    window.location = "doguineo.html";
+}
+
+function sala() {
+    window.location = "sala.html";
+}
+
+function livro() {
+    window.location = "livro.html";
+}
+
+function quarto1() {
+    window.location = "quarto.html";
+}
